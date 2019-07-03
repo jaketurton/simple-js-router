d91ac7b79478b59fd14f3551bf36f8c4bfb53a1a
Jake Turton <unpkjake@gmail.com>
2019-07-03 15:32:41 +0100

    initial commit, basic router setup

---
+++ gruntfile.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = function (grunt) {
+    
+    const sass = require('node-sass');
+    grunt.initConfig({
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'public/css/styles.css': 'scss/styles.scss'
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true,
+              },
+            scripts: {
+                files: ['scss/*.scss'],
+                tasks: ['sass'],
+            },
+          },
+      });
+      
+      grunt.loadNpmTasks('grunt-sass');
+      grunt.loadNpmTasks('grunt-contrib-watch');
+
+      grunt.registerTask('default', ['sass']);
+
+};
