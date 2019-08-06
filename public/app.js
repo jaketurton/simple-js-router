d2a16519c6086633ccaf2c2a85b1a7b14170cabd
Jake Turton <unpkjake@gmail.com>
2019-08-06 14:48:37 +0100

    minor refactor

--- public/app.js
+++ public/app.js
@@ -1,62 +1,52 @@
 
 window.addEventListener('DOMContentLoaded', function() {
 
-    var Cache = {
-        stack: {}, //Cache stack
-        load: function(id){ //Load cache if found
-            return (typeof(this.stack[id]) != 'undefined') ? this.stack[id] : false;
+    let Cache = {
+        storage: {},
+        load: function(id){
+            return (typeof(this.storage[id]) != 'undefined') ? this.storage[id] : false;
         },
-        save: function(data,id){ //Cache data with unique id
-            this.stack[id] = data;
-        },
-        remove: function(id){ //Remove cache for identifier
-            if(typeof(this.stack[id]) != 'undefined')
-                delete this.stack[id];
+        save: function(data,id){
+            this.storage[id] = data;
         }
     };
 
-    var view = document.getElementById('view');
-    var currentPath = window.location.pathname;
+    let view = document.getElementById('view');
+    let currentPath = window.location.pathname;
 
-    fetchContent(currentPath);
+    getContent(currentPath);
 
     function changeContent(content) {
         view.innerHTML = content;
 
-        var activeRoutes = Array.from(document.querySelectorAll('[route]'));
+        let activeRoutes = Array.from(document.querySelectorAll('.navigation'));
 
         activeRoutes.forEach(function(route) {
             route.addEventListener('click', navigateOnClick, false);
         });
     }
 
-    function fetchContent(route) {
-        // default route
-        if (route == "/") {
+    function getContent(route) {
+        if (route == "/") { // todo: dont live in here
             route = "/one";
         }
 
-        //We check the cache first.
-        var content = Cache.load('page' + route);
+        let content = Cache.load('page' + route);
 
-        //If cache is empty, it will return false.
         if(content == false) {
-            //Retrieve the cache via AJAX request using jQuery
             fetch('/views' + route + '.html', {})
             .then( res => res.text())
             .then((data) => {
 
                 if (route != '/four')
                 {
-                    //Save the cache, so next time no request is needed.
-                    Cache.save(data,'page' + route);
+                    Cache.save(data,'page' + route); // todo: dont live in here
                 }
 
                 console.log('NOT loaded from cache 😢');
                 changeContent(data);
             });
 
-        //Cache hit, load the content immediately
         } else {
             changeContent(content);
             console.log('Loaded from cache ✨');
@@ -64,20 +54,27 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     function pushRouteToWindowHistory(currentPath) {
-        //console.log(currentPath, window.location.origin + currentPath);
         window.history.pushState({}, currentPath, window.location.origin + currentPath);
     }
 
-    function navigateOnClick(e) {
-        var route = event.target.attributes[0].value;
-        fetchContent(route);
-        pushRouteToWindowHistory(route);
+    function changeView(route) {
+        getContent(route);
 
-        currentPath = route;
+        // promise
+        // to do: on success changecontent()
+        // else error
+
+        pushRouteToWindowHistory(route);
+    }
+
+    function navigateOnClick(e) {
+        e.preventDefault();
+        const route = e.target.pathname;
+        changeView(route);
     };
 
     window.addEventListener('popstate', function() {
-        fetchContent(window.location.pathname);
+        changeView(window.location.pathname);
     });
 
 })
